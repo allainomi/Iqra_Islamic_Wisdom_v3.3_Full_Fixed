db7567a84793f8b32116f0f75ecb07ccc7f72408
allainomi <z3448761@gmail.com>
2025-10-12 20:25:51 -0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/iqraislamicwisdompro/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/iqraislamicwisdompro/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.iqraislamicwisdompro
+package [your.package.name] // اپنا اصل پیکیج کا نام یہاں رہنے دیں
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
