3171e097fe18afa25d06a43d653da6c2fc88f187
allainomi <z3448761@gmail.com>
2025-10-14 08:07:17 +0500

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/iqraislamicwisdompro/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/iqraislamicwisdompro/MainActivity.kt
@@ -1,4 +1,4 @@
-package [your.package.name] // اپنا اصل پیکیج کا نام یہاں رہنے دیں
+package com.iqra.iqra_islamic_wisdom  // اپنا package name استعمال کریں
 
 import io.flutter.embedding.android.FlutterActivity
 
