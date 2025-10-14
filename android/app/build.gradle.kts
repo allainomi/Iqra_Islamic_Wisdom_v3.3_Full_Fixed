29917cd379f2fed5b2535538ad707a380932a6ce
allainomi <z3448761@gmail.com>
2025-10-14 08:09:00 +0500

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,23 +1,31 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
 android {
-    namespace = "com.example.iqraislamicwisdompro"
-    compileSdk = 34
+    namespace "com.iqra.iqra_islamic_wisdom"  // اپنا package name
+    compileSdkVersion 33
 
-    ndkVersion = "27.0.12077973"
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = '1.8'
+    }
+
+    sourceSets {
+        main.java.srcDirs += 'src/main/kotlin'
+    }
 
     defaultConfig {
-        applicationId = "com.example.iqraislamicwisdompro"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        applicationId "com.iqra.iqra_islamic_wisdom"  // اپنا package name
+        minSdkVersion 21
+        targetSdkVersion 33
+        versionCode 1
+        versionName "3.3.0"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            signingConfig signingConfigs.debug
+        }
+    }
+}
